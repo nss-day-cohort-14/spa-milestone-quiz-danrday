4813e79b3e857583ac68b99b4ef61c5ad6301f37
Dan Day <danday@gmail.com>
2016-06-17 23:46:52 -0500

    commented out code

--- activateElements.js
+++ activateElements.js
@@ -1,55 +1,46 @@
 var Carlot = (function (carlot) {
 
+  carlot.activateElements = function () {
+    //adds event listeners to everything at once
+    var allCards = document.getElementsByClassName("singleCard");
+    for (i = 0; i < allCards.length; i++) {
+    allCards[i].addEventListener("click", carlot.clickHandler);
+    };
+  };
 
-  //need to define these globaly so that crazyFunction can run
-  var selectedTargetNum;
-  var descriptionSelected;
+  carlot.clickHandler = function() {
+
+    textBox.value = ""; //resets the textbox
+
+    //finds div the user clicked in, gets its number
+    selectedTargetNum = event.target.closest("div").id.split("--")[1];
+
+    //locate the description section of card content
+    var selectedDescriptionID = `description--${selectedTargetNum}`;
+
+    //select description section of card content
+    descriptionSelected = document.getElementById(selectedDescriptionID);
+
+    //adds event listener to textBox to update its value to the description in real time when a key is pressed
+    textBox.focus();
+    textBox.addEventListener("keyup", editDescript);
+  };
+
+  //defined these globally so that editDescript() updates DOM immediately
+  var selectedTargetNum;  //number corresponding to array value in JSON file
+  var descriptionSelected; //
   var textBox = document.getElementById("textInput");
 
-
-
   function editDescript(e) {
+      //if the user presses enter, re-write card's actual array value to whatever the user has typed.
        if (13 == e.keyCode) {
         carlot.getInventory()[selectedTargetNum].description == textBox.value;
         textBox.value = "";
     }
+      //any key other than "enter" is text output into the description area
       else {descriptionSelected.innerHTML = "Description: " + textBox.value;
     }
     };
-
-  carlot.clickHandler = function() {
-    //finds the parent div of the event target and gets its number in the array
-
-    textBox.value = "";
-
-    selectedTargetNum = event.target.closest("div").id.split("--")[1];
-
-    var selectedDescription = `description--${selectedTargetNum}`;
-
-    descriptionSelected = document.getElementById(selectedDescription);
-
-    textBox.focus();
-
-    // descriptionSelected.innerHTML = textBox.value
-
-    //adds event listener to textBox when a key is pressed
-    textBox.addEventListener("keyup", editDescript);
-  };
-
-
-  carlot.activateElements = function () {
-    var allCards = document.getElementsByClassName("singleCard");
-
-    console.log(allCards.length);
-    console.log(allCards);
-
-    for (i = 0; i < allCards.length; i++) {
-      console.log(allCards[i]);
-
-    allCards[i].addEventListener("click", carlot.clickHandler);
-
-    };
-  };
 
   return carlot;
 })(Carlot || {});
--- quiz.js
+++ quiz.js
@@ -1,10 +1,14 @@
 var Carlot = (function (carlot) {
 
-//holds all information for the cars
+//will hold all JSON info for the cars
 var carArray = [];
 
+//getter for carArray
+carlot.getInventory = function () {
+  return carArray;
+}
 
-
+//loads the inventory and applies a callback function
 carlot.loadInventory = function (callback) {
       var messageLoader = new XMLHttpRequest();
       messageLoader.open("GET", "inventory.json");
@@ -15,6 +19,7 @@ carlot.loadInventory = function (callback) {
       });
     };
 
+//populates variable contentArea and then prints it once to DOM
 carlot.printCards = function() {
   var areaDOMToPrint = document.getElementById("mainDisplay");
 
@@ -28,10 +33,13 @@ carlot.printCards = function() {
       contentArea += `<div class="row">`
      };
 
+     //add card properties and ID
     contentArea += `<div class="col-sm-3 singleCard" id="carProducts--${i}">`;
 
+    //main card content
     contentArea += `<p>Make: ${currCard.make}</p><p>Model: ${currCard.model}</p><p>Year: ${currCard.year}</p><p>Price: ${currCard.price}</p><p>Color: ${currCard.color}</p><p>Purchased: ${currCard.purchased}</p><p id="description--${i}">Description: ${currCard.description}</p>`;
 
+    //closing card properties and ID div
     contentArea += `</div>`
 
     // closes row div on every 6th card
@@ -41,18 +49,15 @@ carlot.printCards = function() {
 
   };
 
+  //ready to print to DOM
   areaDOMToPrint.innerHTML = contentArea;
 
-  //load event listeners
-
+  //after printing to DOM, load event listeners
   carlot.activateElements();
 
 };
 
-carlot.getInventory = function () {
-  return carArray;
-}
-
+//loads up the inventory and prints the cards
 carlot.loadInventory(carlot.printCards);
 
 
